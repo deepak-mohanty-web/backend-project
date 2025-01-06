ddb8c53c5bc0d3a36a6b6204a539418df530f712
Deepak Mohanty <mohantydeepak284@gmail.com>
2025-01-07 00:11:44 +0530

    change

--- index.js
+++ index.js
@@ -16,13 +16,12 @@ app.listen(process.env.PORT || 5000, () => {
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to database");
   })
   .catch((err) => {
-    console.error("Database connection error:", err.message); 
+    console.error("Database connection error:", err.message);
   });
 
 app.get("/", async (req, res) => {
